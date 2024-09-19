68c447d4ea306c9fa8aaf235827a4d0fe5d46c24
Aryan-kamboj-31 <aryankamboj31@gmail.com>
2024-09-19 12:00:07 +0530

    add code

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',   // Use environment variables for flexibility
     user: process.env.DB_USER || 'root',        // Default to 'root', but use env variables if available
     password: process.env.DB_PASS || '',        // Add password if needed
-    database: process.env.DB_NAME || 'aryan',   // Use env variable for database name
+    database: process.env.DB_NAME || 'sharan',   // Use env variable for database name
     waitForConnections: true,                   // Manage pool behavior
     connectionLimit: 10,                        // Limit the number of connections in the pool
     queueLimit: 0
